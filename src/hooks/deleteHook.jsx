093664c4a0b92a2e2d9bdcc5575b583aed4426c8
ayalashraiber <ayalash44@gmail.com>
2024-03-26 16:18:22 +0200

    bacik_server_calls

---
+++ src/hooks/deleteHook.jsx
@@ -0,0 +1,16 @@
+
+import axios from 'axios'
+
+const UseDelete = () => {
+
+    const Delete = async (url) => {
+        try {
+            await axios.delete(url)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+    return Delete
+}
+
+export default UseDelete
