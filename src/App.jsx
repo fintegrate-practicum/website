865f6fa70ec204c1142e38e7f1c80985f8b7a452
eb59162 <stb1641@gmail.com>
2024-03-26 16:46:50 +0200

    Redux to web site

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { store } from './Redux/Store'
+import { Provider } from 'react-redux';
 
 function App() {
   const [count, setCount] = useState(0)
