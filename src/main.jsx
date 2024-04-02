3b4d3573e46d7aa17996e1b0004ada5140cb6819
tziviYaffe <leahy0135@gmail.com>
2024-04-02 09:58:53 +0300

    update file main.jsx-Removing comments

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,3 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-// import './index.css'
-// import {Auth0Provider} from '@auth0/auth0-react';
-
-// const domain = 'dev-wqjhr1dlimiej1zb.us.auth0.com';
-// const clientId = 'RQUpz9kKDedJ5EeaLNdnZmlT1bQ9pUhl';
-
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//      <Auth0Provider
-//     domain={domain}
-//     clientId={clientId}
-//     redirectUri={window.location.origin}>
-//     <App />
-//     </Auth0Provider>
-//   </React.StrictMode>,
-// )
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
