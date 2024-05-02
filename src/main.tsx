781cae4288feaa56b72e6bfd9bc6525f0433d355
Rachel <r0504108994@gmail.com>
2024-05-02 17:10:03 +0300

    after change

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,10 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App'
-// import './index.css'
-// // import { BrowserRouter } from 'react-router-dom'
-// import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//     <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-// )
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+
 
 const rootElement = document.getElementById('root');
 
