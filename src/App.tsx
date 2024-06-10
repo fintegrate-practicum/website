cf479a0f401eefe597b700eb499185408d959aba
ayalale <0527679694a@gmail.com>
2024-06-10 17:04:21 +0300

    resulve

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,28 @@ import { useState } from 'react';
 import menuItem from '../src/components/menu/types';
 import LazyRouter from './components/router/lazyRouter';
 import AuthMenu from './auth0/AuthMenu';
+import * as iconsMaterial from '@mui/icons-material';
+import SideMenu from './components/menu/SideMenu';
+
+const menuItems = [
+  
+  {
+    name: 'settings',
+    nameToView: 'Settings',
+    icon: iconsMaterial.Settings,
+    route: '../Setting/Category',
+  },
+  {
+    name: 'Header',
+    nameToView: 'Headerofile',
+    icon: iconsMaterial.Person,
+    route: '../Header/Header',
+  },
+];
 
 function App() {
 
-  const [currentMenu, setCurrentMenu] = useState<menuItem>();
+  const [currentMenu, setCurrentMenu] = useState<menuItem>(menuItems[1]);
 
   return (
     <>
@@ -18,6 +36,7 @@ function App() {
       <ThemeProvider theme={theme}>
         <Provider store={Store}>
           <div></div>
+          <SideMenu items={menuItems} setCurrentMenu={setCurrentMenu} />
           <LazyRouter currentRoute={currentMenu?.route || ' '} />
           <div>Hello</div>
         </Provider>
