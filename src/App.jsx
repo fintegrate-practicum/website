e1941b387af2c5a81f6ec1d5aa24baf5ebde8407
ayalashraiber <ayalash44@gmail.com>
2024-06-15 23:10:37 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SideebarWorkerDetails from './componnents/sideebar-work-basic-details'
-import { ThemeProvider } from '@mui/material/styles';
-import { Provider } from 'react-redux';
-import './App.css';
-import { store } from './Redux/Store';
-import theme from './Theme';
- 
 
 function App() {
   return (
     <>
  sidebar-component
       <SideebarWorkerDetails/>
-      <ThemeProvider theme={theme}>
-        <Provider store={store}>
-
-        </Provider>
-      </ThemeProvider>
- 
     </>
   )
 }
--- src/componnents/sideebar-work-basic-details.jsx
+++ src/componnents/sideebar-work-basic-details.jsx
@@ -6,61 +6,52 @@ import AddIcon from '@mui/icons-material/Add';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
 export default function SideebarWorkerDetails() {
-    const [state, setState] = React.useState(false);
-    const ANCHOR = 'right';
-    const [details_arr, setDetails_arr]=React.useState(['name', 'phon', 'email','details'])
-    const [icons_arr, setIcons_arr] = React.useState([<AccountBoxIcon />, <CallIcon />, <AddIcon />, <MailIcon />]);
+  const [state, setState] = React.useState(false);
+  const ANCHOR = 'right';
 
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setState(open);
+  };
 
-    const toggleDrawer = (open) => (event) => {
-        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-            return;
-        }
-        setState(open);
-    };
-
-    const list = () => (
-        <Box
-            sx={{ 'auto': 250 }}
-            role="presentation"
-            onClick={toggleDrawer(false)}
-            onKeyDown={toggleDrawer(false)}
-        >
+  return (
+    <div>
+      <React.Fragment key='right'>
+        <Button onClick={toggleDrawer(true)}>details</Button>
+        <Drawer anchor={ANCHOR} open={state} onClose={toggleDrawer(false)}>
+          <Box sx={{ 'auto': 250 }} role="presentation">
             <List>
-                {details_arr.map((text, index) => (
-                    <>
-               
-                        <ListItem key={index} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    {icons_arr[index]}
-                                {/* {index===0?<AccountBoxIcon/>: index === 1 ? <CallIcon />: index === 3 ? <AddIcon/> :<MailIcon />} */}
-                                </ListItemIcon>
-                                <ListItemText primary={text} />
-                            </ListItemButton>
-                        </ListItem>
-                    </>
-
-                ))}
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon><AccountBoxIcon /></ListItemIcon>
+                  <ListItemText primary="name" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon><CallIcon /></ListItemIcon>
+                  <ListItemText primary="phon" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon><MailIcon /></ListItemIcon>
+                  <ListItemText primary="email" />
+                </ListItemButton>
+              </ListItem>
+              <ListItem disablePadding>
+                <ListItemButton>
+                  <ListItemIcon><AddIcon /></ListItemIcon>
+                  <ListItemText primary="details " />
+                </ListItemButton>
+              </ListItem>
+              <Divider />
             </List>
-            <Divider />
-        </Box>
-    );
-
-    return (
-        <div>
-                <React.Fragment key='right'>
-                    <Button onClick={toggleDrawer(true)}>details</Button>
-                    <Drawer
-                        anchor={ANCHOR}
-                        open={state}
-                        onClose={toggleDrawer(false)}
-                    >
-                        {list()}
-                    </Drawer>
-                </React.Fragment>
-
-        </div>
-
-    );
+          </Box>
+        </Drawer>
+      </React.Fragment>
+    </div>
+  );
 }
