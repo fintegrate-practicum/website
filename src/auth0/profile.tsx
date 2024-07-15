915995f0449ed1c3e7884216e32ecb49febfa2b9
chanyAshlag <ca0556704745@gmail.com>
2024-07-15 09:41:14 +0300

    the format

--- src/auth0/profile.tsx
+++ src/auth0/profile.tsx
@@ -11,33 +11,33 @@ import { Link } from 'react-router-dom';
 const auth0_audience = import.meta.env.VITE_AUTH0_AUDIENCE as string;
 const auth0_domain = import.meta.env.VITE_AUTH0_DOMAIN as string;
 
-const Profile: React.FC = () => {  
+const Profile: React.FC = () => {
   const { user, isAuthenticated, isLoading, getAccessTokenSilently } = useAuth0();
-  const [userMetadata, setUserMetadata] = useState<any>(null); 
+  const [userMetadata, setUserMetadata] = useState<any>(null);
   const dispatch = useAppDispatch()
 
-  function setCookie(name:String, value:String, days:number) {
+  function setCookie(name: String, value: String, days: number) {
     let expires = "";
     if (days) {
-        const date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-       expires = `; expires=${date.toUTCString()}`;
-      }
+      const date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = `; expires=${date.toUTCString()}`;
+    }
     document.cookie = name + "=" + (value || "") + expires + "; path=/";
   }
-  useEffect(() => {  
-      
+  useEffect(() => {
+
     const getUserMetadata = async () => {
       const domain = auth0_domain;
       try {
-        const accessToken = await getAccessTokenSilently({      
+        const accessToken = await getAccessTokenSilently({
           authorizationParams: {
             audience: auth0_audience,
             scope: "read:current_user",
           },
-          
+
         });
-        setCookie("accessToken",accessToken,7)
+        setCookie("accessToken", accessToken, 7)
         const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user?.sub}`;
         const metadataResponse = await fetch(userDetailsByIdUrl, {
           headers: {
@@ -45,8 +45,8 @@ const Profile: React.FC = () => {
           },
         });
         const user_metadata = await metadataResponse.json();
-        setUserMetadata(user_metadata);        
-        dispatch(fetchUserById(user_metadata?.user_id));        
+        setUserMetadata(user_metadata);
+        dispatch(fetchUserById(user_metadata?.user_id));
       } catch (e) {
         console.log((e as Error).message);
       }
@@ -55,7 +55,7 @@ const Profile: React.FC = () => {
     if (user?.sub) {
       getUserMetadata();
     }
-  }, [getAccessTokenSilently, user?.sub,dispatch]);
+  }, [getAccessTokenSilently, user?.sub, dispatch]);
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -63,14 +63,14 @@ const Profile: React.FC = () => {
 
   const profileAvatar = () => {
     let emailUser = '';
-    if(userMetadata){
+    if (userMetadata) {
       emailUser = userMetadata.email;
     }
     return {
       sx: {
         bgcolor: 'red',
-        position: 'relative', 
-        width: 37, 
+        position: 'relative',
+        width: 37,
         height: 37
       },
       children: emailUser ? `${emailUser.split('')[0][0]}${emailUser.split('')[1][0]}` : ''
@@ -91,16 +91,17 @@ const Profile: React.FC = () => {
     <>
       {isAuthenticated && (
         <div>
-        <Box 
-          position="absolute" 
-          top={18} 
-          right={18} 
-          onClick={handleClick}
-          sx={{ cursor: 'pointer' }} 
-        >
-          <Avatar {...profileAvatar()} src={userMetadata?.picture || ''} />    
-        </Box>
-         <Link to={'/CreateBusiness/BaseDetailsManager'}>הרשמה של עסק</Link></div>
+          <Box
+            position="absolute"
+            top={18}
+            right={18}
+            onClick={handleClick}
+            sx={{ cursor: 'pointer' }}
+          >
+            <Avatar {...profileAvatar()} src={userMetadata?.picture || ''} />
+          </Box>
+          <Link to={'/CreateBusiness/BaseDetailsManager'}>הרשמה של עסק</Link>
+        </div>
       )}
       <SidebarUserDetails
         email={userMetadata?.email}
