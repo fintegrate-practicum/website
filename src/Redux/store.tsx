0f7cfb55f8f5a8d2964ec5927e10e6a42f3aa2fa
efchbd1013 <efchbd1013@gmail.com>
2024-07-15 10:43:40 +0300

    updated3

--- src/Redux/store.tsx
+++ src/Redux/store.tsx
@@ -1,11 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import businessSlice from './businessSlice'
 import currentUserSlice from "./currentUserSlice";
+import { userSlice } from "../modules/workers/features/reducer";
+import employeeSlice from "../modules/workers/features/employeeSlice";
+import taskSlice from "../modules/workers/features/taskSlice";
+import messageSlice from "../modules/workers/features/messageSlice";
 
 const Store = configureStore({
     reducer: {
         businessSlice,
         currentUserSlice,
+        user:userSlice.reducer,
+        employeeSlice,
+        taskSlice,
+        messageSlice,
+
     }
 })
 
