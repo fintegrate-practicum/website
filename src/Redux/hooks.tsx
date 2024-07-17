7d6cc5998002a89fe0d8bf5ad800aa6b086d834b
michalzrian <m43797@gmail.com>
2024-07-17 11:27:26 +0300

    add middleware to store

--- src/Redux/hooks.tsx
+++ src/Redux/hooks.tsx
@@ -5,24 +5,16 @@ import type { RootState, AppDispatch } from './store';
 export const useJwtFromCookie = (cookieName: string): string | null => {
     
     const cookies = document.cookie;
-    
-   
     const cookieArray = cookies.split(';');
-    
-   
+
     for (let cookie of cookieArray) {
-    
         cookie = cookie.trim();
-        
-      
-        if (cookie.startsWith(`${cookieName}=`)) {
-           
+        if (cookie.startsWith(`${cookieName}=`)) {     
             return cookie.substring(cookieName.length + 1);
         }
-    }
-    
-    
+    }  
     return null;
 }
+
 export const useAppDispatch = () => useDispatch<AppDispatch>();
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
--- src/Redux/store.tsx
+++ src/Redux/store.tsx
@@ -1,23 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
-import employeeSlice from "./employeeSlice";
-import taskSlice from "./taskSlice";
-import messageSlice from "./messageSlice";
-import userSlice from './reducer'
 import businessSlice from './businessSlice'
 import currentUserSlice from "./currentUserSlice";
+import { userSlice } from "../modules/workers/features/reducer";
+import employeeSlice from "../modules/workers/features/employeeSlice";
+import taskSlice from "../modules/workers/features/taskSlice";
+import messageSlice from "../modules/workers/features/messageSlice";
 
 const Store = configureStore({
     reducer: {
-        user: userSlice,
         businessSlice,
+        currentUserSlice,
+        user: userSlice.reducer,
         employeeSlice,
         taskSlice,
         messageSlice,
-        currentUserSlice,
-
-    }
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: false,
+        }),
 })
 
-export type RootState = ReturnType<typeof Store.getState>
-export type AppDispatch = typeof Store.dispatch
+
+
+export type RootState = ReturnType<typeof Store.getState>;
+export type AppDispatch = typeof Store.dispatch;
 export default Store;
