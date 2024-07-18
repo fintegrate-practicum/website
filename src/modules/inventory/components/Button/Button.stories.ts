0d03e5117d9ca0c22627d17b42bbeec87267bcb5
st0534 <estypwolak>
2024-07-18 22:27:20 +0300

    fix the mistakes of the Button

---
+++ src/modules/inventory/components/Button/Button.stories.ts
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import theme from '../../../../../src/Theme';
+import Button from './Button';
+
+const meta = {
+    component: Button,
+    tags: ['autodocs'],
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const fullColor: Story = {
+    args: {
+        backgroundColor: theme.palette.secondary.dark,
+        borderColor: theme.palette.primary.dark,
+        border:"double",
+        outlineColor: theme.palette.info.main,
+        color: theme.palette.info.main,
+        value: "Button",
+        isLink:false
+        
+    },
+
+};
+export const empty: Story = {
+    args: {
+        value: "Button",
+        backgroundColor: "white",
+        borderColor: theme.palette.primary.dark,
+        border:"double",
+        outlineColor: theme.palette.info.main,
+        color: theme.palette.secondary.dark,
+        isLink:false
+    },
+
+};
+export const link: Story = {
+    args: {
+        value: "Button",
+        backgroundColor: "white",
+        borderColor: theme.palette.primary.dark,
+        border:"double",
+        outlineColor: theme.palette.info.main,
+        color: theme.palette.secondary.dark,
+        isLink:true
+    },
+
+};
---
+++ src/modules/inventory/components/Button/Button.tsx
@@ -0,0 +1,39 @@
+
+import MaterialButton from '@mui/material/Button';
+import Link from '@mui/material/Link';
+interface ButtonProps {
+    onClickFunction: () => void;
+    href:string,
+    isLink: boolean;
+    value: string | number;
+    backgroundColor: '#F2B704' | '#F2CB05' | '#6503A6'
+    borderColor: '#F2B704' | '#F2CB05' | '#6503A6',
+    border: string,
+    outlineColor: '#F2B704' | '#F2CB05' | '#6503A6',
+    color: '#F2B704' | '#F2CB05' | '#6503A6'
+}
+const Button = (props: ButtonProps) => {
+if(props.isLink){
+return (
+    <Link href={props.href}>Link</Link>
+)
+}
+else{
+    return (
+            <MaterialButton onClick={props.onClickFunction}
+                style={{
+                    backgroundColor: props.backgroundColor,
+                    borderColor: props.borderColor,
+                    border: props.border,
+                    outlineColor: props.outlineColor,
+                    color: props.color
+                }}
+            >
+                {props.value},
+                {props.isLink}
+            </MaterialButton>
+    )
+}
+   
+}
+export default Button;
