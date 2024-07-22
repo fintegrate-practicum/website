03b7330cd34b2859fb7194a4addf167c0644b970
c_s <c0533158787@gmail.com>
2024-07-22 14:45:39 +0300

    fixing the whole code and adding husky - pre-commit

--- src/modules/orders/reportWebVitals.ts
+++ src/modules/orders/reportWebVitals.ts
@@ -1,15 +1,15 @@
-import {ReportOpts } from 'web-vitals';
+// import {ReportOpts } from 'web-vitals';
 
 
-const reportWebVitals = (onPerfEntry?: ReportOpts) => {
-  if (onPerfEntry && typeof onPerfEntry === 'function') {
-    import('web-vitals').then(({ onCLS, onINP, onFCP, onLCP }) => {
-      onCLS(onPerfEntry);
-      onINP(onPerfEntry);
-      onFCP(onPerfEntry);
-      onLCP(onPerfEntry);
-    }).catch(error => console.error('Failed to load web-vitals', error));
-  }
-};
+// const reportWebVitals = (onPerfEntry?: ReportOpts) => {
+//   if (onPerfEntry && typeof onPerfEntry === 'function') {
+//     import('web-vitals').then(({ onCLS, onINP, onFCP, onLCP }) => {
+//       onCLS(onPerfEntry);
+//       onINP(onPerfEntry);
+//       onFCP(onPerfEntry);
+//       onLCP(onPerfEntry);
+//     }).catch(error => console.error('Failed to load web-vitals', error));
+//   }
+// };
 
-export default reportWebVitals;
+// export default reportWebVitals;
