92c46d9237887f699b44873c053fa89d6fc8514f
475michal <166406013+475michalk@users.noreply.github.com>
2024-07-24 17:03:35 +0300

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,26 @@
 import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react'
-import eslintPlugin from 'vite-plugin-eslint'
+import react from '@vitejs/plugin-react';
+import eslintPlugin from 'vite-plugin-eslint';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     eslintPlugin({
       cache: false,
-      include: ['./src/**/*.js', './src/**/*.jsx'],
+      include: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
       exclude: [],
     }),
   ],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: '0.0.0.0', // אפשרות זו מאפשרת גישה מחוץ לקונטיינר
+    port: 5173,
+  },
   test: {
     environment: 'jsdom',
     globals: true,
     setupFiles: './src/tests/setup.ts',
   }
-})
+});
