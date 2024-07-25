7286c8a734de928e65808d6e01a041f180d5dbab
TamarVilner <144704638+TamarVilner@users.noreply.github.com>
2024-07-25 15:22:20 +0300

    Update IProduct.ts

--- src/modules/inventory/interfaces/IProduct.ts
+++ src/modules/inventory/interfaces/IProduct.ts
@@ -2,10 +2,6 @@ export interface IProduct {
     id: string;
     name: string;
     description: string;
-    images: string[];
-    name: string;
-    description: string;
-    images: string[];
     packageCost: number;
     productComponents: string[];
     totalPrice: number;
@@ -15,8 +11,10 @@ export interface IProduct {
     salePercentage: number;
     stockQuantity: number;
     businessId: string;
-    businessId: string;
     componentStatus: string;
 }
+
+
+
 
 
