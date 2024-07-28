a288ccf2ee20afd49d1c246d97b03a42494cbdb1
Racheli <c0504197194@gmail.com>
2024-07-28 14:49:37 +0300

    add a getcookie setcookie and get a token from getAccessTokenSilently by passing the userID parameter

--- src/auth0/profile.tsx
+++ src/auth0/profile.tsx
@@ -24,25 +24,31 @@ const Profile: React.FC = () => {
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = `; expires=${date.toUTCString()}`;
     }
-    
-    document.cookie = name + "=" + (value || "") + expires + "; path=/; HttpOnly";
-    // Secure
-    
-}
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
+  function getCookie(name: string) {
+    const nameEQ = name + "="; const ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) === ' ') c = c.substring(1, c.length); if (c.indexOf(nameEQ) === 0)
+        return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
 
-  useEffect(() => {  
-      
+  setCookie("user_id", user?.sub as string, 30);
+  useEffect(() => {
     const getUserMetadata = async () => {
       const domain = auth0_domain;
       try {
-        const accessToken = await getAccessTokenSilently({      
+        const accessToken = await getAccessTokenSilently({
           authorizationParams: {
+            userId:getCookie("user_id"),
             audience: auth0_audience,
             scope: "read:current_user",
           },
           
         });
-        setCookie("accessToken",accessToken,7)
         const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user?.sub}`;
         const metadataResponse = await fetch(userDetailsByIdUrl, {
           headers: {
