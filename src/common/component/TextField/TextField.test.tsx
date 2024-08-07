cbd54821b91257aba47ed5ee81297febeba5d8b5
chanyAshlag <ca0556704745@gmail.com>
2024-08-07 16:18:57 +0300

    change test

--- src/common/component/TextField/TextField.test.tsx
+++ src/common/component/TextField/TextField.test.tsx
@@ -4,18 +4,18 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import TextField from './TextField';
 
 test('renders TextField component without crashing', () => {
-  render(<TextField id="test-id" variant="outlined" label="Test Label" value="" onChange={() => {}} disabled={false} margin="normal" InputProps={{}} sx={{}} />);
+  render(<TextField />);
 });
 
 test('renders TextField component and checks if it is displayed with its label', () => {
-  render(<TextField id="test-id" variant="outlined" label="Test Label" value="" onChange={() => {}} disabled={false} margin="normal" InputProps={{}} sx={{}} />);
+  render(<TextField />);
   const textFieldElement = screen.getByLabelText('Test Label');
   expect(textFieldElement).toBeInTheDocument();
 });
 
 test('verifies that events are activated on TextField component', () => {
   const mockOnChange = jest.fn();
-  render(<TextField id="test-id" variant="outlined" label="Test Label" value="" onChange={mockOnChange} disabled={false} margin="normal" InputProps={{}} sx={{}} />);
+  render(<TextField />);
   const textFieldElement = screen.getByLabelText('Test Label');
   textFieldElement.focus();
   fireEvent.change(textFieldElement, { target: { value: 'Test Value' } });
