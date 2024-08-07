ccd6c09b1a5e1ca898e9bfe1744498902dd3acdd
michalzrian <m43797@gmail.com>
2024-08-07 17:31:23 +0300

    toast_storybook with test

---
+++ src/stories/Toast.stories.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import Toast, { ToastProps } from './Toast';
+
+export default {
+	title: 'Components/Toast',
+	component: Toast,
+} as Meta;
+
+const Template: StoryFn<ToastProps> = (args) => {
+	const [open, setOpen] = useState(args.open);
+
+	const handleClose = () => {
+		setOpen(false);
+		action('onClose')();
+	};
+
+	return <Toast {...args} open={open} onClose={handleClose} />;
+};
+
+export const Success = Template.bind({});
+Success.args = {
+	message: 'This is a success message!',
+	severity: 'success',
+	open: true,
+	duration: 4000,
+};
+
+export const Info = Template.bind({});
+Info.args = {
+	message: 'This is an info message!',
+	severity: 'info',
+	open: true,
+	duration: 4000,
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+	message: 'This is a warning message!',
+	severity: 'warning',
+	open: true,
+	duration: 4000,
+};
+
+export const Error = Template.bind({});
+Error.args = {
+	message: 'This is an error message!',
+	severity: 'error',
+	open: true,
+	duration: 4000,
+};
---
+++ src/stories/Toast.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Snackbar, Alert, IconButton } from '@mui/material';
+import {
+	CheckCircle as CheckCircleIcon,
+	Info as InfoIcon,
+	Warning as WarningIcon,
+	Error as ErrorIcon,
+	Close as CloseIcon,
+} from '@mui/icons-material';
+
+export interface ToastProps {
+	message: string;
+	severity?: 'success' | 'info' | 'warning' | 'error';
+	open: boolean;
+	onClose: () => void;
+	duration?: number;
+}
+
+const Toast: React.FC<ToastProps> = ({
+	message,
+	severity = 'info',
+	open,
+	onClose,
+	duration = 6000,
+}) => {
+	const getSeverityIcon = (
+		severity: 'success' | 'info' | 'warning' | 'error',
+	) => {
+		switch (severity) {
+			case 'success':
+				return <CheckCircleIcon className='toastIcon' />;
+			case 'info':
+				return <InfoIcon className='toastIcon' />;
+			case 'warning':
+				return <WarningIcon className='toastIcon' />;
+			case 'error':
+				return <ErrorIcon className='toastIcon' />;
+			default:
+				return null;
+		}
+	};
+
+	return (
+		<div className='toastContainer'>
+			<Snackbar open={open} autoHideDuration={duration} onClose={onClose}>
+				<Alert
+					onClose={onClose}
+					severity={severity}
+					iconMapping={{
+						success: getSeverityIcon('success'),
+						info: getSeverityIcon('info'),
+						warning: getSeverityIcon('warning'),
+						error: getSeverityIcon('error'),
+					}}
+					action={
+						<IconButton
+							size='small'
+							aria-label='close'
+							color='inherit'
+							onClick={onClose}
+						>
+							<CloseIcon fontSize='small' />
+						</IconButton>
+					}
+				>
+					{message}
+				</Alert>
+			</Snackbar>
+		</div>
+	);
+};
+
+export default Toast;
