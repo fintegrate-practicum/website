2ede2f7aa7b6179b38b826d26cb801817efb0498
chanyAshlag <ca0556704745@gmail.com>
2024-08-07 12:31:59 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,40 +6,50 @@ import theme from './Theme';
 import Client from './components/client/Client';
 import MainRouter from './components/router/MainRouter';
 import React, { Suspense, useEffect, useState } from 'react';
-import {  Route, Routes, useLocation } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import { useAppSelector } from './Redux/hooks';
 import ErrorToast, { showErrorToast } from './components/generic/errorMassage';
 import Inventory from './modules/inventory/Inventory';
 import Login from './components/Login/login';
 import Orders from './modules/orders/App';
 import Header from './components/Header/Header';
+import { useTranslation } from 'react-i18next';
+import LanguageSwitcher from './components/LanguageSwitcher/LanguageSwitcher';
 
 const LazyEditProfile = React.lazy(() => import('./auth0/editProfile'));
-const LazyBaseDetailsManager = React.lazy(() => import('./components/createBusiness/baseDetailsManager'));
-const LazyEmailVerification = React.lazy(() => import('./components/createBusiness/emailVerification'));
-const LazyMoreDetailsManager = React.lazy(() => import('./components/createBusiness/moreDetailsManager'));
+const LazyBaseDetailsManager = React.lazy(
+  () => import('./components/createBusiness/baseDetailsManager'),
+);
+const LazyEmailVerification = React.lazy(
+  () => import('./components/createBusiness/emailVerification'),
+);
+const LazyMoreDetailsManager = React.lazy(
+  () => import('./components/createBusiness/moreDetailsManager'),
+);
 const LazyClient = React.lazy(() => import('./components/client/Client'));
 
-
-
 const App = () => {
-  const currentUser = useAppSelector((state) => state.currentUserSlice.CurrentUser);
+  const { t } = useTranslation();
+  const currentUser = useAppSelector(
+    (state) => state.currentUserSlice.CurrentUser,
+  );
   const [typeUser, setTypeUser] = useState<any | null>(null);
   const [lastInvalidPath, setLastInvalidPath] = useState<string | null>(null);
   const location = useLocation();
 
   useEffect(() => {
     if (currentUser) {
-      const type = currentUser.employeeDetails.role.type
+      const type = currentUser.employeeDetails.role.type;
       setTypeUser(type);
     }
   }, [currentUser]);
 
   const ErrorToastRoute = () => {
-    
     useEffect(() => {
       if (location.pathname !== lastInvalidPath) {
-        showErrorToast('הדף שאת/ה מחפש/ת אינו נמצא route-הכנס/י ב http://localhost:0000/link/**של עסק linkUID**');
+        showErrorToast(
+          'הדף שאת/ה מחפש/ת אינו נמצא route-הכנס/י ב http://localhost:0000/link/**של עסק linkUID**',
+        );
         setLastInvalidPath(location.pathname);
       }
     }, [location, lastInvalidPath]);
@@ -51,21 +61,61 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <Provider store={Store}>
-      <Header />
+        <Header />
         <Client />
+        <LanguageSwitcher />
         <ErrorToast />
         <Routes>
-          <Route path="/inventory/*" element={<Inventory />} />
+          <Route path='/inventory/*' element={<Inventory />} />
 
-          <Route path="/editProfile" element={<Suspense fallback="Loading..."><LazyEditProfile /></Suspense>} />
-          <Route path="/CreateBusiness/BaseDetailsManager" element={<Suspense fallback="Loading..."><LazyBaseDetailsManager /></Suspense>} />
-          <Route path="/CreateBusiness/EmailVerification" element={<Suspense fallback="Loading..."><LazyEmailVerification /></Suspense>} />
-          <Route path="/CreateBusiness/MoreDetailsManager" element={<Suspense fallback="Loading..."><LazyMoreDetailsManager /></Suspense>} />
-          <Route path="/link/:linkUID" element={<Suspense fallback="Loading..."><LazyClient /></Suspense>} />
+          <Route
+            path='/editProfile'
+            element={
+              <Suspense fallback={t('Loading...')}>
+                <LazyEditProfile />
+              </Suspense>
+            }
+          />
+          <Route
+            path='/CreateBusiness/BaseDetailsManager'
+            element={
+              <Suspense fallback={t('Loading...')}>
+                <LazyBaseDetailsManager />
+              </Suspense>
+            }
+          />
+          <Route
+            path='/CreateBusiness/EmailVerification'
+            element={
+              <Suspense fallback={t('Loading...')}>
+                <LazyEmailVerification />
+              </Suspense>
+            }
+          />
+          <Route
+            path='/CreateBusiness/MoreDetailsManager'
+            element={
+              <Suspense fallback={t('Loading...')}>
+                <LazyMoreDetailsManager />
+              </Suspense>
+            }
+          />
+          <Route
+            path='/link/:linkUID'
+            element={
+              <Suspense fallback={t('Loading...')}>
+                <LazyClient />
+              </Suspense>
+            }
+          />
         </Routes>
         {isRootPath && (
           <>
-            {typeUser !== 'manager' && typeUser !== 'admin' && typeUser !== '' && typeUser !== undefined && typeUser !== null ? (
+            {typeUser !== 'manager' &&
+            typeUser !== 'admin' &&
+            typeUser !== '' &&
+            typeUser !== undefined &&
+            typeUser !== null ? (
               <Client />
             ) : typeUser === 'manager' || typeUser === 'admin' ? (
               <>
@@ -79,5 +129,5 @@ const App = () => {
       </Provider>
     </ThemeProvider>
   );
-}
+};
 export default App;
