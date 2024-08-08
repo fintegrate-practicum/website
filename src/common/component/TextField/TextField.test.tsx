66981337223877ae16712a60ced48e590b5df39c
chanyAshlag <ca0556704745@gmail.com>
2024-08-08 13:21:54 +0300

    test

--- src/common/component/TextField/TextField.test.tsx
+++ src/common/component/TextField/TextField.test.tsx
@@ -1,23 +1,12 @@
-// import  jest  from '@jest/globals'; 
+// import  jest  from '@jest/globals';
 import '@testing-library/jest-dom';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import TextField from './TextField';
-
 test('renders TextField component without crashing', () => {
-  render(<TextField />);
+	render(<TextField />);
 });
-
 test('renders TextField component and checks if it is displayed with its label', () => {
-  render(<TextField />);
-  const textFieldElement = screen.getByLabelText('Test Label');
-  expect(textFieldElement).toBeInTheDocument();
-});
-
-test('verifies that events are activated on TextField component', () => {
-  const mockOnChange = jest.fn();
-  render(<TextField />);
-  const textFieldElement = screen.getByLabelText('Test Label');
-  textFieldElement.focus();
-  fireEvent.change(textFieldElement, { target: { value: 'Test Value' } });
-  expect(mockOnChange).toHaveBeenCalledTimes(1);
+	render(<TextField label='Test Label' />);
+	const textFieldElement = screen.getByLabelText('Test Label');
+	expect(textFieldElement).toBeInTheDocument();
 });
