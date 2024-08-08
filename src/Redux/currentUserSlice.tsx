60172a468e513b1b0aca3c3f67faf24d63229cdb
Tami-Co <158340919+Tami-Co@users.noreply.github.com>
2024-08-08 10:37:53 +0300

    changes in cuurentUserSlice

--- src/Redux/currentUserSlice.tsx
+++ src/Redux/currentUserSlice.tsx
@@ -3,7 +3,7 @@ import { RootState } from "./store";
 import { EmployeeRole } from "../modules/workers/classes/employeeRole";
 import { statuses } from "../modules/workers/classes/enum/statuses.enum";
 import { showErrorToast } from "../components/generic/errorMassage";
-import InfraInterceptors from '../auth0/InfraInterceptors'
+import InfraInstance from "../auth0/InfraInterceptors";
 
 const initialState = {
   CurrentUser: {
@@ -40,11 +40,15 @@ const initialState = {
 
 export const fetchUserById = createAsyncThunk(
   'fetchUserById',
-  async (userId: string, { dispatch }) => {
+  async (paylod: any, { dispatch }) => {
     try {
-      const response = await InfraInterceptors.get(`$/currentUser/${userId}`);
-      const data = response.data;   
-      dispatch(currentUserSlice.actions.setCurrentUser(data));      
+      const response = await InfraInstance.get(`/user/${paylod.identities[0].user_id}`);
+      const data = response.data;
+      if(data.data==null)
+      {
+         await dispatch(updateCurrentUserByJwt(paylod))
+      }
+      dispatch(currentUserSlice.actions.setCurrentUser(data));
       return data;
     } catch (error: any) {
       showErrorToast(error.message);
@@ -55,14 +59,25 @@ export const fetchUserById = createAsyncThunk(
 export const updateCurrentUser = createAsyncThunk('', async (payload: any) => { 
     const { auth0_user_id, updatedCurrentUser } = payload;
     try {           
-        const response = await InfraInterceptors.put(`$/currentUser/${auth0_user_id}`, updatedCurrentUser);
-        return response.data;
-    } catch (error:any) {
-      showErrorToast(error.message);
-    }
+      const response = await InfraInstance.put(`/user/${auth0_user_id}`, updatedCurrentUser);
+      return response.data;
+  } catch (error: any) {
+    showErrorToast(error.message);
+  }
 }
 )
 
+export const updateCurrentUserByJwt = createAsyncThunk(
+  'updateCurrentUserByJwt',
+  async (payload: any) => {
+    try {
+      const response = await InfraInstance.put('/user/jwt', payload,);
+      return response.data;
+    } catch (error: any) {
+      throw error
+    }
+  }
+)
 const currentUserSlice = createSlice({
   name: "CurrentUser",
   initialState,
