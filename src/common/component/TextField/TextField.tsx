0b7b5ceae5b91ce0d796afc32c2d1272a808c64d
chanyAshlag <ca0556704745@gmail.com>
2024-08-08 13:13:53 +0300

    onChange

--- src/common/component/TextField/TextField.tsx
+++ src/common/component/TextField/TextField.tsx
@@ -1,41 +1,57 @@
+import React, { useState, useEffect } from 'react';
 import { TextField } from '@mui/material';
-import React from 'react';
-interface TextFieldProps  {
-  id?: string;
-  variant?: "filled" | "outlined" | "standard";
-  label?: string;
-  value?: any;
-  onChange?: React.ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>;
-  disabled?: boolean;
-  margin?: "none" | "dense" | "normal";
-  InputProps?: Partial<React.ComponentProps<typeof TextField>>;
-  sx?: object;
+
+interface TextFieldProps {
+	id?: string;
+	variant?: 'filled' | 'outlined' | 'standard';
+	label?: string;
+	value?: string;
+	onChange?: React.ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement>;
+	disabled?: boolean;
+	margin?: 'none' | 'dense' | 'normal';
+	InputProps?: Partial<React.ComponentProps<typeof TextField>>;
+	sx?: object;
 }
 
-  const CustomTextField = (props: React.ComponentProps<typeof TextField> & TextFieldProps) => {
-  const {
-    id,
-    variant = 'outlined',
-    label,
-    value,
-    onChange,
-    disabled = false,
-    margin = 'normal',
-    InputProps,
-    sx,
-  } = props;
-  return (
-    <TextField
-      id={id}
-      variant={variant}
-      label={label}
-      value={value}
-      onChange={onChange}
-      disabled={disabled}
-      margin={margin}
-      InputProps={InputProps}
-      sx={sx}
-    />
-  );
+const CustomTextField: React.FC<
+	React.ComponentProps<typeof TextField> & TextFieldProps
+> = ({
+	id,
+	variant = 'outlined',
+	label,
+	value = '',
+	onChange,
+	disabled = false,
+	margin = 'normal',
+	InputProps,
+	sx,
+}) => {
+	const [internalValue, setInternalValue] = useState(value);
+
+	useEffect(() => {
+		setInternalValue(value);
+	}, [value]);
+
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		setInternalValue(event.target.value);
+		if (onChange) {
+			onChange(event);
+		}
+	};
+
+	return (
+		<TextField
+			id={id}
+			variant={variant}
+			label={label}
+			value={internalValue}
+			onChange={handleChange}
+			disabled={disabled}
+			margin={margin}
+			InputProps={InputProps}
+			sx={sx}
+		/>
+	);
 };
+
 export default CustomTextField;
