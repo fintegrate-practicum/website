2e6dbf527d7ddf5551728b4fe9161e62b95c5393
Efrat Grinboim <151626406+Efrat6821@users.noreply.github.com>
2024-08-08 17:40:45 +0300

    employee view 2

--- src/modules/workers/features/employeeSlice.tsx
+++ src/modules/workers/features/employeeSlice.tsx
@@ -1,8 +1,9 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { RootState } from "../../../Redux/store";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { RootState } from '../../../Redux/store';
 
-import axios from "axios";
-import employee from "../classes/employee";
+import axios from 'axios';
+import employee from '../classes/employee';
+import workerInstance from '../../../auth0/WorkersInterceptors';
 
 const baseUrl = import.meta.env.VITE_WORKERS_SERVICE_URL;
 const businessId = import.meta.env.VITE_BUSINESSID;
@@ -10,38 +11,53 @@ const res = await axios.get(`${baseUrl}/workers?businessId=${businessId}`);
 const { data = {} } = res.data;
 
 const employeeSlice = createSlice({
-    name: "employees",
-    initialState: data,
-    reducers: {}
-})
+  name: 'employees',
+  initialState: data,
+  reducers: {},
+});
 
-export const { } = employeeSlice.actions;
-export const selectEmployees = (state :RootState) => state.employeeSlice.employees
+export const selectEmployees = (state: RootState) =>
+  state.employeeSlice.employees;
 export default employeeSlice.reducer;
 
 export const addEmployee = createAsyncThunk('', async (_employee: employee) => {
-    try {
-        const response = await axios.post(`${baseUrl}/workers`, _employee);
-        return response.data
-    } catch (error) {
-        return error
-    }
+  try {
+    const response = await workerInstance.post(`/workers`, _employee);
+    return response.data;
+  } catch (error) {
+    console.error('error', error);
+  }
 });
 
 export const deleteEmployee = createAsyncThunk('', async (_num: number) => {
-    try {
-        const response = await axios.delete(`${baseUrl}/workers/${_num}`)
-        return response.data
-    } catch (error) {
-        return error
-    }
+  try {
+    const response = await workerInstance.delete(`/workers/${_num}`);
+    return response.data;
+  } catch (error) {
+    console.error('error', error);
+  }
 });
 
-export const editEmployee = createAsyncThunk('', async (_employee: employee) => {
+export const editEmployee = createAsyncThunk(
+  '',
+  async (_employee: employee) => {
     try {
-        const response = await axios.put(`${baseUrl}/workers/${_employee.userId}`, _employee)
-        return response.data
+      const response = await workerInstance.put(
+        `/workers/${_employee.userId}`,
+        _employee,
+      );
+      return response.data;
     } catch (error) {
-        return error
+      console.error('error', error);
     }
+  },
+);
+
+export const getUserById = createAsyncThunk('', async (id: string) => {
+  try {
+    const response = await axios.get(`${baseUrl}/user/${id}`);
+    return response.data.data;
+  } catch (error) {
+    return error;
+  }
 });
