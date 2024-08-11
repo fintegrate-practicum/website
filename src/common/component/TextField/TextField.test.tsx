0d078a8b2e224a0f70da124fd5783022fe8fd09f
chanyAshlag <ca0556704745@gmail.com>
2024-08-11 10:01:16 +0300

    test

--- src/common/component/TextField/TextField.test.tsx
+++ src/common/component/TextField/TextField.test.tsx
@@ -1,15 +1,3 @@
-// import  jest  from '@jest/globals';
-// import '@testing-library/jest-dom';
-// import { render, screen } from '@testing-library/react';
-// import TextField from './TextField';
-// test('renders TextField component without crashing', () => {
-// 	render(<TextField />);
-// });
-// test('renders TextField component and checks if it is displayed with its label', () => {
-// 	render(<TextField label='Test Label' />);
-// 	const textFieldElement = screen.getByLabelText('Test Label');
-// 	expect(textFieldElement).toBeInTheDocument();
-// });
 import '@testing-library/jest-dom';
 import { render, screen, fireEvent } from '@testing-library/react';
 import TextField from './TextField';
