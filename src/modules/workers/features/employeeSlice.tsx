b06dc3e20eeebf4cd7f17fd2bc908e3cd6606612
noah-cohen <n0548548106@gmail.com>
2024-08-11 11:18:51 +0200

    solving_problem

--- src/modules/workers/features/employeeSlice.tsx
+++ src/modules/workers/features/employeeSlice.tsx
@@ -1,12 +1,11 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../../Redux/store";
-
+import infraInstance from '../../../auth0/InfraInterceptors'
 import axios from "axios";
 import employee from "../classes/employee";
 
-const baseUrl = import.meta.env.VITE_WORKERS_SERVICE_URL;
 const businessId = import.meta.env.VITE_BUSINESSID;
-const res = await axios.get(`${baseUrl}/workers?businessId=${businessId}`);
+const res = await axios.get(`${infraInstance}/workers?businessId=${businessId}`);
 const { data = {} } = res.data;
 
 const employeeSlice = createSlice({
@@ -21,7 +20,7 @@ export default employeeSlice.reducer;
 
 export const addEmployee = createAsyncThunk('', async (_employee: employee) => {
     try {
-        const response = await axios.post(`${baseUrl}/workers`, _employee);
+        const response = await axios.post(`${infraInstance}/workers`, _employee);
         return response.data
     } catch (error) {
         return error
@@ -30,7 +29,7 @@ export const addEmployee = createAsyncThunk('', async (_employee: employee) => {
 
 export const deleteEmployee = createAsyncThunk('', async (_num: number) => {
     try {
-        const response = await axios.delete(`${baseUrl}/workers/${_num}`)
+        const response = await axios.delete(`${infraInstance}/workers/${_num}`)
         return response.data
     } catch (error) {
         return error
@@ -39,7 +38,7 @@ export const deleteEmployee = createAsyncThunk('', async (_num: number) => {
 
 export const editEmployee = createAsyncThunk('', async (_employee: employee) => {
     try {
-        const response = await axios.put(`${baseUrl}/workers/${_employee.userId}`, _employee)
+        const response = await axios.put(`${infraInstance}/workers/${_employee.userId}`, _employee)
         return response.data
     } catch (error) {
         return error
