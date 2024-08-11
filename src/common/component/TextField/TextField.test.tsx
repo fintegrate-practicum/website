0e71b02341e2e1e097f192a28c7fe21234ff99f7
chanyAshlag <ca0556704745@gmail.com>
2024-08-11 09:42:30 +0300

    test onchange

--- src/common/component/TextField/TextField.test.tsx
+++ src/common/component/TextField/TextField.test.tsx
@@ -1,7 +1,19 @@
 // import  jest  from '@jest/globals';
+// import '@testing-library/jest-dom';
+// import { render, screen } from '@testing-library/react';
+// import TextField from './TextField';
+// test('renders TextField component without crashing', () => {
+// 	render(<TextField />);
+// });
+// test('renders TextField component and checks if it is displayed with its label', () => {
+// 	render(<TextField label='Test Label' />);
+// 	const textFieldElement = screen.getByLabelText('Test Label');
+// 	expect(textFieldElement).toBeInTheDocument();
+// });
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import TextField from './TextField';
+import { vitest } from 'vitest';
 test('renders TextField component without crashing', () => {
 	render(<TextField />);
 });
@@ -9,4 +21,11 @@ test('renders TextField component and checks if it is displayed with its label',
 	render(<TextField label='Test Label' />);
 	const textFieldElement = screen.getByLabelText('Test Label');
 	expect(textFieldElement).toBeInTheDocument();
+});
+test('onChange function is called when input value is changed', () => {
+	const mockOnChange = vitest.fn();
+	render(<TextField label='Test Label' onChange={mockOnChange} />);
+	const textFieldElement = screen.getByLabelText('Test Label');
+	fireEvent.change(textFieldElement, { target: { value: 'Test Value' } });
+	expect(mockOnChange).toHaveBeenCalled();
 });
