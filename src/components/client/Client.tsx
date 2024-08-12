b40c4d8310b50e76db9914b46cfb38652487012f
chanyAshlag <ca0556704745@gmail.com>
2024-08-12 11:12:05 +0300

    client

--- src/components/client/Client.tsx
+++ src/components/client/Client.tsx
@@ -8,117 +8,116 @@ import { useParams, Outlet } from 'react-router-dom';
 import { showErrorToast } from '../generic/errorMassage';
 import { useTranslation } from 'react-i18next';
 interface Business {
-	id: string;
-	companyNumber: string;
-	description: string;
-	name: string;
-	email: string;
-	logo: string;
-	phone: string;
-	owner: string;
-	businessSize: string;
-	industryType: string;
-	establishmentDate: string;
-	linkUID: string;
-	createdAt: string;
-	updatedAt: string;
-	__v: number;
+  id: string;
+  companyNumber: string;
+  description: string;
+  name: string;
+  email: string;
+  logo: string;
+  phone: string;
+  owner: string;
+  businessSize: string;
+  industryType: string;
+  establishmentDate: string;
+  linkUID: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
 }
 
 export default function LazyClient() {
-	const { linkUID } = useParams<{ linkUID: string }>();
-	const [business, setBusiness] = useState<Business | null>(null);
-	const [loading, setLoading] = useState(true);
-	const [errorOccurred, setErrorOccurred] = useState(false);
-	const baseUrl = import.meta.env.VITE_INFRA_SERVICE_URL;
+  const { t } = useTranslation();
+  const { linkUID } = useParams<{ linkUID: string }>();
+  const [business, setBusiness] = useState<Business | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [errorOccurred, setErrorOccurred] = useState(false);
+  const baseUrl = import.meta.env.VITE_INFRA_SERVICE_URL;
 
-	useEffect(() => {
-		async function fetchBusinessData() {
-			try {
-				console.log(`Fetching business data for linkUID: ${linkUID}`);
-				const response = await axios.get(`${baseUrl}/business/link/${linkUID}`);
-				console.log('Business data fetched successfully:', response.data);
-				setBusiness(response.data);
-			} catch (error) {
-				console.error('Error fetching business data', error);
-				setErrorOccurred(true);
-			} finally {
-				setLoading(false);
-			}
-		}
-		if (linkUID) {
-			fetchBusinessData();
-		} else {
-			setLoading(false);
-		}
-	}, [linkUID]);
+  useEffect(() => {
+    async function fetchBusinessData() {
+      try {
+        console.log(`Fetching business data for linkUID: ${linkUID}`);
+        const response = await axios.get(`${baseUrl}/business/link/${linkUID}`);
+        console.log('Business data fetched successfully:', response.data);
+        setBusiness(response.data);
+      } catch (error) {
+        console.error('Error fetching business data', error);
+        setErrorOccurred(true);
+      } finally {
+        setLoading(false);
+      }
+    }
+    if (linkUID) {
+      fetchBusinessData();
+    } else {
+      setLoading(false);
+    }
+  }, [linkUID]);
 
-	useEffect(() => {
-		if (errorOccurred) {
-			showErrorToast(
-				'הדף שאת/ה מחפש/ת אינו נמצא route-הכנס/י ב http://localhost:0000/link/**של עסק linkUID**',
-			);
-		}
-	}, [errorOccurred]);
+  useEffect(() => {
+    if (errorOccurred) {
+      showErrorToast(
+        'הדף שאת/ה מחפש/ת אינו נמצא route-הכנס/י ב http://localhost:0000/link/**של עסק linkUID**',
+      );
+    }
+  }, [errorOccurred]);
 
-	if (loading) {
-		return <Typography>Loading...</Typography>;
-	}
+  if (loading) {
+    return <Typography>{t('common.Loading...')}</Typography>;
+  }
 
-	if (!business) {
-		return null;
-	}
+  if (!business) {
+    return null;
+  }
 
-	const { t } = useTranslation();
-
-	return (
-		<>
-			<Box
-				sx={{
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center',
-					height: '50vh',
-					flexDirection: 'column',
-					textAlign: 'right',
-				}}
-			>
-				<Typography variant='h5'>{t('website.Business Details')}</Typography>
-				<Box sx={{ mb: 2 }}>
-					<Typography>
-						{business.name} : {t('website.Business Name')}
-					</Typography>
-					<Typography>
-						{business.companyNumber} : {t('website.Company Number')}
-					</Typography>
-					<Typography>
-						{business.description} : {t('website.Description')}
-					</Typography>
-					<Typography>
-						{business.email} : {t('website.Email')}
-					</Typography>
-					<Typography>
-						{business.phone} : {t('website.Phone')}
-					</Typography>
-					<Typography>
-						{business.owner} : {t('website.Owner')}
-					</Typography>
-					<Typography>
-						{business.businessSize} : {t('website.Business Size')}
-					</Typography>
-					<Typography>
-						{business.industryType} : {t('website.Industry Type')}
-					</Typography>
-					<Typography>
-						{new Date(business.establishmentDate).toLocaleDateString()} :{' '}
-						{t('website.Establishment Date')}
-					</Typography>
-				</Box>
-				<Stack spacing={2} direction='row'>
-					<Button>{t('website.Create Order')}</Button>
-				</Stack>
-			</Box>
-			<Outlet />
-		</>
-	);
+  return (
+    <>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '50vh',
+          flexDirection: 'column',
+          textAlign: 'right',
+        }}
+      >
+        <Typography variant='h5'>{t('website.Business Details')}</Typography>
+        <Box sx={{ mb: 2 }}>
+          <Typography>
+            {business.name} : {t('website.Business Name')}
+          </Typography>
+          <Typography>
+            {business.companyNumber} : {t('website.Company Number')}
+          </Typography>
+          <Typography>
+            {business.description} : {t('website.Description')}
+          </Typography>
+          <Typography>
+            {business.email} : {t('website.Email')}
+          </Typography>
+          <Typography>
+            {business.phone} : {t('website.Phone')}
+          </Typography>
+          <Typography>
+            {business.owner} : {t('website.Owner')}
+          </Typography>
+          <Typography>
+            {business.businessSize} : {t('website.Business Size')}
+          </Typography>
+          <Typography>
+            {business.industryType} : {t('website.Industry Type')}
+          </Typography>
+          <Typography>
+            {new Date(business.establishmentDate).toLocaleDateString()} :{' '}
+            {t('website.Establishment Date')}
+          </Typography>
+        </Box>
+        <Stack spacing={2} direction='row'>
+          <Button>{t('website.Create Order')}</Button>
+        </Stack>
+      </Box>
+      <Outlet />
+    </>
+  );
 }
