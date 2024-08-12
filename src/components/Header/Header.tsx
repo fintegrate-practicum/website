d5a3b362e79d8bfab742d297a454aa7cb1550161
chanyAshlag <ca0556704745@gmail.com>
2024-08-12 11:08:48 +0300

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,6 +6,7 @@ import Typography from '../../common/components/Typography/Typography';
 import IconButton from '@mui/material/IconButton';
 import ContactMailIcon from '@mui/icons-material/ContactMail';
 import SettingsIcon from '@mui/icons-material/Settings';
+import { useTranslation } from 'react-i18next';
 // will be constructed during use
 // import Avatar from '@mui/material/Avatar';
 // import { useSelector } from 'react-redux';
@@ -13,43 +14,45 @@ import SettingsIcon from '@mui/icons-material/Settings';
 // import { Link } from 'react-router-dom';
 
 export interface HeaderProps {
-	serviceName?: string;
-	children?: ReactElement;
+  serviceName?: string;
+  children?: ReactElement;
 }
 
 const Header: FC<HeaderProps> = ({
-	serviceName,
-	children,
+  serviceName,
+  children,
 }: HeaderProps): ReactElement => {
-	// To retrieve a user after creating the state
-	// const user = useSelector((state: RootState) => state.user.currentUser);
-	return (
-		<Box sx={{ flexGrow: 1 }}>
-			<AppBar position='static' sx={{ width: '100%' }}>
-				<Toolbar>
-					<img src='/logo.png' alt='Logo' style={{ width: '15%' }} />
+  const { t } = useTranslation();
 
-					<Typography variant='h6' component='div' style={{ flexGrow: 1 }}>
-						{serviceName || 'Send service name in props'}
-					</Typography>
+  // To retrieve a user after creating the state
+  // const user = useSelector((state: RootState) => state.user.currentUser);
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position='static' sx={{ width: '100%' }}>
+        <Toolbar>
+          <img src='/logo.png' alt='Logo' style={{ width: '15%' }} />
 
-					{/* {user && <Avatar alt="User Avatar" src={user.imageUrl} />} */}
+          <Typography variant='h6' component='div' style={{ flexGrow: 1 }}>
+            {serviceName || t('website.Send service name in props')}
+          </Typography>
 
-					{/* Please add the appropriate routing inside the tag of each button in this format
+          {/* {user && <Avatar alt="User Avatar" src={user.imageUrl} />} */}
+
+          {/* Please add the appropriate routing inside the tag of each button in this format
               component={Link} to="/yourRoute"
               according to the routes that will exist
           */}
-					{children}
-					<IconButton color='inherit' aria-label='Contact Mail'>
-						<ContactMailIcon />
-					</IconButton>
-					<IconButton color='inherit' aria-label='Settings'>
-						<SettingsIcon />
-					</IconButton>
-				</Toolbar>
-			</AppBar>
-		</Box>
-	);
+          {children}
+          <IconButton color='inherit' aria-label='Contact Mail'>
+            <ContactMailIcon />
+          </IconButton>
+          <IconButton color='inherit' aria-label='Settings'>
+            <SettingsIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 };
 
 export default Header;
