098392e16f090ed0636d6ca2375bbe8c69f9de4b
c_s <c0533158787@gmail.com>
2024-08-12 13:29:51 +0300

    remove an unnaccesary line

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,7 +25,6 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
   settings: {
