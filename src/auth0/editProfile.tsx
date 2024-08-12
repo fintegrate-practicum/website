5fa2e784d054f1ecd74e7193d4734b556c07d8c5
chanyAshlag <ca0556704745@gmail.com>
2024-08-12 10:48:08 +0300

    editP

--- src/auth0/editProfile.tsx
+++ src/auth0/editProfile.tsx
@@ -1,63 +1,65 @@
-import React, { useState } from 'react';
+import React from 'react';
+import TextField from '../common/component/TextField/TextField';
 import {
-  TextField,
   Box,
-  Grid,
-  Paper,
-  SelectChangeEvent,
-  Tooltip,
+  Button,
   FormControl,
+  Grid,
   InputLabel,
-  Select,
   MenuItem,
+  Paper,
+  Select,
+  Tooltip,
 } from '@mui/material';
 import Typography from '../common/components/Typography/Typography';
-import Button from '../common/components/Button/Button';
 import IconButton from '@mui/material/IconButton';
 import InputAdornment from '@mui/material/InputAdornment';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import { useAppDispatch, useAppSelector } from '../Redux/hooks';
 import { updateCurrentUser } from '../Redux/currentUserSlice';
 import { statuses } from '../modules/workers/classes/enum/statuses.enum';
+import { Controller, useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
-// Convert enum to array of strings
 const statusArray = Object.keys(statuses).filter((key) => isNaN(Number(key)));
+
 const EditProfile: React.FC = () => {
   const { t } = useTranslation();
-  const currentUser = useAppSelector((state) => state.currentUserSlice);
-  const [isEditing, setIsEditing] = useState(false);
-  const [status, setStatus] = useState(String(currentUser.userDetails.status));
-  const dispatch = useAppDispatch();
-  const [formData, setFormData] = useState({
-    email: currentUser.userDetails.userEmail || '',
-    name: currentUser.userDetails.userName || '',
-    role: currentUser.employeeDetails.role.type || '',
+  const { control, setValue, getValues } = useForm({
+    defaultValues: {
+      email: '',
+      name: '',
+      role: '',
+      status: '',
+    },
   });
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
-  const handleChangeStatus = (event: SelectChangeEvent) => {
-    setStatus(event.target.value as string);
-  };
+  const currentUser = useAppSelector((state) => state.currentUserSlice);
+  const [isEditing, setIsEditing] = React.useState(false);
+  const dispatch = useAppDispatch();
+
+  React.useEffect(() => {
+    setValue('email', currentUser.userDetails.userEmail || '');
+    setValue('name', currentUser.userDetails.userName || '');
+    setValue('role', currentUser.employeeDetails.role.type || '');
+    setValue('status', String(currentUser.userDetails.status));
+  }, [currentUser, setValue]);
+
   const handleEditClick = () => {
     setIsEditing(true);
   };
+
   const handleSaveClick = () => {
     setIsEditing(false);
     const updatedCurrentUser = {
       employee: {
         ...currentUser.employeeDetails,
-        role: { ...currentUser.employeeDetails.role, type: formData.role },
+        role: { ...currentUser.employeeDetails.role, type: getValues('role') },
       },
       user: {
         ...currentUser.userDetails,
-        userEmail: formData.email,
-        userName: formData.name,
-        status: status,
+        userEmail: getValues('email'),
+        userName: getValues('name'),
+        status: getValues('status'),
       },
     };
     const newData = {
@@ -66,9 +68,11 @@ const EditProfile: React.FC = () => {
     };
     dispatch(updateCurrentUser(newData));
   };
+
   const handleCopy = () => {
-    navigator.clipboard.writeText(formData.email);
+    navigator.clipboard.writeText(getValues('email'));
   };
+
   return (
     <Box sx={{ display: 'flex', justifyContent: 'center', mt: 5 }}>
       <Paper sx={{ p: 4, maxWidth: 600, width: '100%' }}>
@@ -77,74 +81,92 @@ const EditProfile: React.FC = () => {
         </Typography>
         <Grid container spacing={2}>
           <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              label={t('auth0.Email')}
+            <Controller
               name='email'
-              value={formData.email}
-              onChange={handleChange}
-              disabled={true}
-              variant='outlined'
-              margin='normal'
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position='end'>
-                    <Tooltip title={t('auth0.Copy')}>
-                      <IconButton onClick={handleCopy}>
-                        <ContentCopyIcon />
-                      </IconButton>
-                    </Tooltip>
-                  </InputAdornment>
-                ),
-              }}
-              sx={{ mt: 2 }}
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  fullWidth
+                  label={t('auth0.Email')}
+                  disabled
+                  variant='outlined'
+                  margin='normal'
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position='end'>
+                        <Tooltip title={t('auth0.Copy')}>
+                          <IconButton onClick={handleCopy}>
+                            <ContentCopyIcon />
+                          </IconButton>
+                        </Tooltip>
+                      </InputAdornment>
+                    ),
+                  }}
+                  sx={{ mt: 2 }}
+                />
+              )}
             />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              label={t('auth0.Name')}
+            <Controller
               name='name'
-              value={formData.name}
-              onChange={handleChange}
-              disabled={!isEditing}
-              variant='outlined'
-              margin='normal'
-              sx={{ mt: 2 }}
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  fullWidth
+                  label={t('auth0.Name')}
+                  disabled={!isEditing}
+                  variant='outlined'
+                  margin='normal'
+                  sx={{ mt: 2 }}
+                />
+              )}
             />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <FormControl fullWidth sx={{ mt: 2 }}>
-              <InputLabel id='demo-simple-select-label'>
-                {t('auth0.Status')}
-              </InputLabel>
-              <Select
-                labelId='demo-simple-select-label'
-                id='demo-simple-select'
-                value={status}
-                label={t('auth0.Status')}
-                onChange={handleChangeStatus}
-                disabled={!isEditing}
-              >
-                {statusArray.map((statusOption, index) => (
-                  <MenuItem key={index} value={statusOption}>
-                    {statusOption}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
+            <Controller
+              name='status'
+              control={control}
+              render={({ field }) => (
+                <FormControl fullWidth sx={{ mt: 2 }}>
+                  <InputLabel id='status-select-label'>
+                    {t('auth0.Status')}
+                  </InputLabel>
+                  <Select
+                    {...field}
+                    labelId='status-select-label'
+                    id='status-select'
+                    value={field.value}
+                    label={t('auth0.Status')}
+                    disabled={!isEditing}
+                  >
+                    {statusArray.map((statusOption, index) => (
+                      <MenuItem key={index} value={statusOption}>
+                        {statusOption}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              )}
+            />
           </Grid>
           <Grid item xs={12} sm={6}>
-            <TextField
-              fullWidth
-              label={t('auth0.Role')}
+            <Controller
               name='role'
-              value={formData.role}
-              onChange={handleChange}
-              disabled={!isEditing}
-              variant='outlined'
-              margin='normal'
-              sx={{ mt: 2 }}
+              control={control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  fullWidth
+                  label={t('auth0.Role')}
+                  disabled={!isEditing}
+                  variant='outlined'
+                  margin='normal'
+                  sx={{ mt: 2 }}
+                />
+              )}
             />
           </Grid>
         </Grid>
@@ -159,4 +181,5 @@ const EditProfile: React.FC = () => {
     </Box>
   );
 };
+
 export default EditProfile;
