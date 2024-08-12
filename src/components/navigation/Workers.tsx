36184323bac66032df4ea61c12be94270d65c24a
chanyAshlag <ca0556704745@gmail.com>
2024-08-12 11:20:58 +0300

    workers

--- src/components/navigation/Workers.tsx
+++ src/components/navigation/Workers.tsx
@@ -1,30 +1,31 @@
-import React from 'react';
+import * as React from 'react';
 import { Box, Tab } from '@mui/material';
 import { TabContext, TabList, TabPanel } from '@mui/lab';
 import WorkerPage from '../../modules/workers/components/workerPage';
+import TasksShowList from '../../modules/workers/components/tasks/tasksShowList';
 import User from '../../modules/workers/classes/user';
 import employee from '../../modules/workers/classes/employee';
 import MessageList from '../../modules/workers/components/messageList';
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
+import Task from '../../modules/workers/classes/task';
 import { useAppDispatch, useAppSelector } from '../../Redux/hooks';
-import { useTranslation } from 'react-i18next'; // Assuming you are using react-i18next for translations
 
 const WorkersTopNav = () => {
-  const { t } = useTranslation();
   const location = useLocation();
-  // const tasks = useAppSelector((state) => state.taskSlice.tasks);
+  const tasks = useAppSelector((state) => state.taskSlice.tasks);
   const messages = useAppSelector((state) => state.messageSlice.messages);
   const currentUser = useAppSelector(
     (state) => state.currentUserSlice.employeeDetails,
   );
   const dispatch = useAppDispatch();
-  // const [setFilteredTasks] = useState<Task[]>(tasks);
+  const [filteredTasks, setFilteredTasks] = useState<Task[]>(tasks);
 
   const [value, setValue] = useState(location.pathname.slice(8));
 
   useEffect(() => {
     setValue(location.pathname.slice(8));
+    console.log(filteredTasks);
   }, [currentUser, dispatch]);
 
   const handleChange = (event: React.SyntheticEvent, newValue: string) => {
@@ -36,28 +37,20 @@ const WorkersTopNav = () => {
       <TabContext value={value}>
         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <TabList onChange={handleChange} aria-label='workers tabs'>
-            <Tab
-              label={t('workers.detailsTab')}
-              value='details'
-              href='details'
-            />
-            <Tab label={t('workers.tasksTab')} value='tasks' href='tasks' />
-            <Tab
-              label={t('workers.messagesTab')}
-              value='messages'
-              href='messages'
-            />
+            <Tab label='details' value='details' href='details' />
+            <Tab label='tasks' value='tasks' href='tasks' />
+            <Tab label='messages' value='messages' href='messages' />
           </TabList>
         </Box>
         <TabPanel value='details'>
           <WorkerPage user={new User()} employee={new employee()} />
         </TabPanel>
-        {/* <TabPanel value='tasks'>
+        <TabPanel value='tasks'>
           <TasksShowList
             filteredTasks={tasks}
             setFilteredTasks={setFilteredTasks}
           />
-        </TabPanel> */}
+        </TabPanel>
         <TabPanel value='messages'>
           <MessageList messages={messages} />
         </TabPanel>
