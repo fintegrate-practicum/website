2f806b3c1437e03c853270f436bddfb4be9904bf
chanyAshlag <ca0556704745@gmail.com>
2024-08-12 09:30:24 +0300

    change login

--- src/auth0/Login.tsx
+++ src/auth0/Login.tsx
@@ -1,14 +1,19 @@
-import { useAuth0 } from "@auth0/auth0-react";
-import Button from '../common/components/Button/Button'
+import { useAuth0 } from '@auth0/auth0-react';
+import Button from '../common/components/Button/Button';
 import { useTranslation } from 'react-i18next';
 
 const LoginButton = () => {
+  const { loginWithRedirect } = useAuth0();
   const { t } = useTranslation();
 
-  const { loginWithRedirect } = useAuth0();
-
-  return  <Button  onClick={() => loginWithRedirect()}>{t("auth0.login")}</Button>;
- 
+  return (
+    <>
+      <Button onClick={() => loginWithRedirect()}>{t('auth0.login')}</Button>;
+      <Button href='/CreateBusiness/BaseDetailsManager' isLink={true}>
+        {t('Register a business')}
+      </Button>
+    </>
+  );
 };
 
 export default LoginButton;
