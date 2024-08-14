5e99a69334e8631820464a1c697fa5a3f5c14244
Tamar Bobby <tamarbobby@gmail.com>
2024-08-14 14:41:14 +0300

    resolve

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,3 @@
-export default {
-    preset: 'ts-jest',
-    testEnvironment: 'jest-environment-jsdom',
-    transform: {
-        "^.+\\.tsx?$": "ts-jest" 
-    // process `*.tsx` files with `ts-jest`
-    },
-    moduleNameMapper: {
-        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
-    },
-}
 
 // jest.config.js
 module.exports = {
