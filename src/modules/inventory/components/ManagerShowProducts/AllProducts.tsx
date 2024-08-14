89a060f968d259ed5ee88b2b7f17a075e3823066
RivkyN <rivkyneumann1223@gmail.com>
2024-08-14 14:37:03 +0300

    Update AllProducts.tsx - אשלק םככ שפן

--- src/modules/inventory/components/ManagerShowProducts/AllProducts.tsx
+++ src/modules/inventory/components/ManagerShowProducts/AllProducts.tsx
@@ -38,7 +38,7 @@ const AllProducts = () => {
 
   const fetchProducts = async () => {
     try {
-      const response = await getAllItems<IProduct[]>('api/inventory/product');
+      const response = await getAllItems<IProduct[]>('inventory/product');
       dispatch(getProducts(response.data));
     } catch (error) {
       console.error(error);
@@ -48,7 +48,7 @@ const AllProducts = () => {
   const fetchComponents = async () => {
     try {
       const response = await getAllItems<IComponent[]>(
-        'api/inventory/component',
+        'inventory/component',
       );
       dispatch(getAllComponents(response.data));
     } catch (error) {
