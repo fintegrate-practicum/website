1a3055dcfee0950e7f64d1d49e5bb53329368016
Tamar Bobby <tamarbobby@gmail.com>
2024-09-01 13:48:29 +0300

    complete_rtl

--- src/components/LanguageSwitcher/LanguageSwitcher.tsx
+++ src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Button, ButtonGroup, Box } from '@mui/material';
+
 const LanguageSwitcher: React.FC = () => {
   const { i18n } = useTranslation();
+
+  // פונקציה לשינוי שפה וכיוון
   const changeLanguage = (lng: string) => {
     if (i18n && typeof i18n.changeLanguage === 'function') {
       i18n.changeLanguage(lng);
+
+      // קביעת הכיוון בהתאם לשפה
+      const newDirection = lng === 'he' ? 'rtl' : 'ltr';
+      document.body.dir = newDirection; // שינוי הכיוון של הדף כולו
     } else {
       console.error('i18n or changeLanguage is not available');
     }
   };
+  
+
   return (
     <Box
       position='fixed'
@@ -20,6 +29,12 @@ const LanguageSwitcher: React.FC = () => {
       borderRadius='4px'
       boxShadow={3}
       p={1}
+      display='flex' // יישור באמצעות Flexbox
+      justifyContent='center' // מרכז את הכפתורים
+      alignItems='center'
+      sx={{
+        direction: 'ltr', // שמור על הכיוון של הכפתורים ב-LTR
+      }}
     >
       <ButtonGroup variant='contained' color='primary'>
         <Button onClick={() => changeLanguage('en')}>English</Button>
@@ -28,4 +43,5 @@ const LanguageSwitcher: React.FC = () => {
     </Box>
   );
 };
+
 export default LanguageSwitcher;
