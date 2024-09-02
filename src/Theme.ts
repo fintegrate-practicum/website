8d6597dd6118688aef95e30eb3319ecc214d58c5
Tamar Bobby <tamarbobby@gmail.com>
2024-09-02 09:58:54 +0300

    delete_something

--- src/Theme.ts
+++ src/Theme.ts
@@ -1,22 +1,3 @@
-// import { createTheme } from '@mui/material/styles';
-
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       main: '#6503A6',
-//       dark: '#380273',
-//     },
-//     secondary: {
-//       main: '#F2CB05',
-//       dark: '#F2B704',
-//     },
-//     info: {
-//       main: '#4F5902',
-//     },
-//   },
-// });
-
-// export default theme;
 import { createTheme } from '@mui/material/styles';
 
 const theme = (direction: any) => createTheme({
