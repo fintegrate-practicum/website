e615574fe65e5556020cca897d43b51e97142b7c
Shifra <sh748168@gmail.com>
2024-09-04 12:17:50 +0300

    update translate

--- src/modules/orders/ShoppingDetails.tsx
+++ src/modules/orders/ShoppingDetails.tsx
@@ -16,28 +16,23 @@ import { ICart } from './interfaces/ICart';
 import { useDispatch } from 'react-redux';
 import { getBasket } from './features/basket/basketSlice';
 import { showErrorToast } from "../../components/generic/errorMassage";
-
+import { useNavigate } from 'react-router-dom';
 interface Props {
     amount: number;
 }
-
 const ShoppingDetails: React.FC<Props> = ({ amount }) => {
-
     const userId = useJwtFromCookie('user_id')?.split('|')[1];
     const token = useJwtFromCookie('accessToken') || undefined;
     const Carts = useAppSelector((state) => state.cart?.data || []);
     const [selectedOption, setSelectedOption] = useState<string>('');
     const dispatch = useDispatch();
-
     const currentEmployee = useAppSelector(
         (state) => state.currentUserSlice.CurrentUser.employeeDetails,
     );
-
     const productsCart = Carts.map(c => ({
         id: c.product.id,
         qty: c.Quantity,
     }));
-
     const getAllCart = async () => {
         try {
             const res = await getAllItems<ICart[]>(`cart/${currentEmployee.businessId}/${userId}`, token);
@@ -47,15 +42,12 @@ const ShoppingDetails: React.FC<Props> = ({ amount }) => {
             console.log(err);
         }
     }
-
     const handleChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
         setSelectedOption(e.target.value);
     };
-
     useEffect(() => {
         getAllCart();
     }, []);
-
     const saveDetails = async (data: Record<string, any>) => {
         try {
             const newData = {
@@ -70,14 +62,14 @@ const ShoppingDetails: React.FC<Props> = ({ amount }) => {
             }
             await addItem("orders", newData);
             reset()
-            alert("Order saved successfully");
+             // todo: change this to use toast from storybook
+            alert("ההזמנה נשמרה בהצלחה");
+
         } catch (err) {
             console.log(err);
-            showErrorToast("Error saving the order");
+            showErrorToast("שגיאה בשמירת ההזמנה");
         }
     };
-
-
     const backForm = yup.object().shape({
         city: yup.string().when((selectedOption) => {
             return selectedOption.toString() === 'delivery' ? yup.string().required('city is a required field') : yup.string();
@@ -98,38 +90,32 @@ const ShoppingDetails: React.FC<Props> = ({ amount }) => {
             return selectedOption.toString() === 'delivery' ? yup.string().required('lastName is a required field') : yup.string()
         }),
     });
-
-
-
     const { register, handleSubmit, reset, formState: { errors } } = useForm({
         resolver: yupResolver(backForm)
     });
-
     return (
         <Grid sx={{ flexGrow: 1 }} container spacing={2} direction="column" alignItems="center">
             <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2 }}>
                 {productsCart.length > 0 && Carts.map((cart) => (
                     <Card sx={{ width: 200, maxWidth: '100%', boxShadow: 'lg', margin: 2 }}>
                         <Typography>
-                            Product Name: {cart.product.name}
+                            שם המוצר: {cart.product.name}
                         </Typography>
                         <Typography>
-                            Quantity: {cart.Quantity}
+                            כמות: {cart.Quantity}
                         </Typography>
                         <Typography>
-                            Price: {cart.product.packageCost}
+                            מחיר: {cart.product.packageCost}
                         </Typography>
                     </Card>
                 ))}
             </Box>
-
             <Typography variant="h5">
-                Total to pay : {amount}
+                סך הכל לתשלום : {amount}
             </Typography>
-
             <form onSubmit={handleSubmit(saveDetails)}>
                 <FormControl >
-                    <Typography >  How would you like to receive the shipment? </Typography>
+                    <Typography >  איך תרצו לקבל את המשלוח? </Typography>
                     <RadioGroup
                         row
                         aria-labelledby="demo-row-radio-buttons-group-label"
@@ -144,33 +130,24 @@ const ShoppingDetails: React.FC<Props> = ({ amount }) => {
                         (<Box sx={{ '& > :not(style)': { m: 1, width: '25ch', display: 'block' }, }}>
                             <TextField id="filled-basic" fullWidth label="עיר" variant="filled" {...register("city")} />
                             {errors.city && <p>{errors.city.message}</p>}
-
                             <TextField id="filled-basic" fullWidth label="רחוב" variant="filled" {...register("street")} />
                             {errors.street && <p>{errors.street.message}</p>}
                             <TextField id="filled-basic" fullWidth label="מספר בנין" variant="filled" type='number' {...register("numBuild")} />
                             {errors.numBuild && <p>{errors.numBuild.message}</p>}
-
                             <TextField id="filled-basic" fullWidth label="קומה" variant="filled" type='number' {...register("floor")} />
                             {errors.floor && <p>{errors.floor.message}</p>}
-
                             <TextField id="filled-basic" fullWidth label="מספר דירה" variant="filled" type='number' {...register("apartmentNumber")} />
                             {errors.apartmentNumber && <p>{errors.apartmentNumber.message}</p>}
-
                             <TextField id="filled-basic" fullWidth label="שם משפחה" variant="filled" {...register("lastName")} />
                             {errors.lastName && <p>{errors.lastName.message}</p>}
-
                         </Box>)
                     }
                     <Button type="submit" variant="contained" color="primary" fullWidth>
-                        save details 
+                        שמירת פרטים
                     </Button>
-
                 </FormControl>
             </form>
         </Grid >
-
     );
 }
-
-
 export default ShoppingDetails;
