2eb177bc28e21e403ee24302958d9aa8bb54c056
Shifra <sh748168@gmail.com>
2024-09-05 21:34:00 +0300

    order flew

--- src/modules/orders/ShoppingDetails.tsx
+++ src/modules/orders/ShoppingDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -6,33 +6,72 @@ import FormControl from '@mui/material/FormControl';
 import * as yup from 'yup';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
 import Button from '../../common/components/Button/Button';
-import { addItem } from './Api-Requests/genericRequests';
+import { addItem, getAllItems } from './Api-Requests/genericRequests';
 import { useTranslation } from 'react-i18next';
+import { useAppSelector, useJwtFromCookie } from '../../Redux/hooks';
+import { ICart } from './interfaces/ICart';
+import { useDispatch } from 'react-redux';
+import { getBasket } from './features/basket/basketSlice';
+import { Typography, Card, TextField, Box, Grid } from '@mui/material';
 import Toast from '../../common/components/Toast/Toast'; 
-const ShoppingDetails = () => {
-  const { t } = useTranslation();
-  const [selectedOption, setSelectedOption] = useState('');
 
+interface Props {
+  amount: number;
+}
+
+const ShoppingDetails: React.FC<Props> = ({ amount }) => {
+  const { t } = useTranslation();
+  const userId = useJwtFromCookie('user_id')?.split('|')[1];
+  const token = useJwtFromCookie('accessToken') || undefined;
+  const Carts = useAppSelector((state) => state.cart?.data || []);
+  const [selectedOption, setSelectedOption] = useState<string>('');
+  const dispatch = useDispatch();
   const [snackbarOpen, setSnackbarOpen] = useState(false);
   const [snackbarMessage, setSnackbarMessage] = useState('');
   const [snackbarSeverity, setSnackbarSeverity] = useState<'success' | 'error'>('success');
 
-  const handleChange = (e: {
-    target: { value: React.SetStateAction<string> };
-  }) => {
+  const currentEmployee = useAppSelector(
+    (state) => state.currentUserSlice.CurrentUser.employeeDetails,
+  );
+
+  const productsCart = Carts.map((c) => ({
+    id: c.product.id,
+    qty: c.Quantity,
+  }));
+
+  const getAllCart = async () => {
+    try {
+      const res = await getAllItems<ICart[]>(`cart/${currentEmployee.businessId}/${userId}`, token);
+      dispatch(getBasket(res.data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedOption(e.target.value);
   };
 
+  useEffect(() => {
+    getAllCart();
+  }, []);
+
   const saveDetails = async (data: Record<string, any>) => {
     try {
-      const response = await addItem('orders', data);
+      const newData = {
+        destinationAddress: { ...data },
+        user: userId,
+        products: productsCart,
+        businessCode: currentEmployee.businessId,
+        settingManeger: 1,
+        deliveryMethod: selectedOption,
+      };
+      await addItem('orders', newData);
+      reset();
       setSnackbarMessage(t('order.Order saved successfully'));
       setSnackbarSeverity('success');
       setSnackbarOpen(true);
-      console.log(response);
     } catch (err) {
       console.log(err);
       setSnackbarMessage(t('order.Error saving the order'));
@@ -42,118 +81,82 @@ const ShoppingDetails = () => {
   };
 
   const backForm = yup.object().shape({
-    city: yup.string().required(t('order.This field is required')),
-    street: yup.string().required(t('order.This field is required')),
-    buildingNumber: yup
-      .number()
-      .typeError(t('order.Enter a building number'))
-      .positive(t('order.Enter a building number')),
-    floor: yup
-      .number()
-      .typeError(t('order.Enter a floor number'))
-      .positive(t('order.Enter a floor number')),
-    apartmentNumber: yup
-      .number()
-      .typeError(t('order.Enter an apartment number'))
-      .positive(t('order.Enter an apartment number')),
-    lastName: yup.string().required(t('order.This field is required')),
-  });
+    city: yup.string().when((selectedOption) => {
+        return selectedOption.toString() === 'delivery' ? yup.string().required('city is a required field') : yup.string();
+    }),
+    street: yup.string().when((selectedOption) => {
+        return selectedOption.toString() === 'delivery' ? yup.string().required('street is a required field') : yup.string()
+    }),
+    numBuild: yup.number().when((selectedOption) => {
+        return selectedOption.toString() === 'delivery' ? yup.number().required('numBuild is a required field') : yup.number()
+    }),
+    floor: yup.number().when((selectedOption) => {
+        return selectedOption.toString() === 'delivery' ? yup.number().required('floor is a required field') : yup.number()
+    }),
+    apartmentNumber: yup.number().when((selectedOption) => {
+        return selectedOption.toString() === 'delivery' ? yup.number().required('apartmentNumber is a required field') : yup.number()
+    }),
+    lastName: yup.string().when((selectedOption) => {
+        return selectedOption.toString() === 'delivery' ? yup.string().required('lastName is a required field') : yup.string()
+    }),
+});
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
+  const { register, handleSubmit, reset, formState: { errors } } = useForm({
     resolver: yupResolver(backForm),
   });
 
   return (
     <>
-      <form onSubmit={handleSubmit(saveDetails)}>
-        <FormControl>
-          <h3>{t('order.How would you like to receive the shipment')}</h3>
-          <RadioGroup
-            row
-            aria-labelledby='demo-row-radio-buttons-group-label'
-            name='row-radio-buttons-group'
-            onChange={handleChange}
-          >
-            <FormControlLabel
-              value='shipping'
-              control={<Radio />}
-              label={t('order.Shipping')}
-            />
-            <FormControlLabel
-              value='selfCollection'
-              control={<Radio />}
-              label={t('order.Self Collection')}
-            />
-          </RadioGroup>
-          {selectedOption === 'shipping' && (
-            <Box
-              sx={{
-                '& > :not(style)': { m: 1, width: '25ch', display: 'block' },
-              }}
+      <Grid sx={{ flexGrow: 1 }} container spacing={2} direction="column" alignItems="center">
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2 }}>
+          {productsCart.length > 0 && Carts.map((cart) => (
+            <Card sx={{ width: 200, maxWidth: '100%', boxShadow: 'lg', margin: 2 }} key={cart.product.id}>
+              <Typography>{t('order.Product Name')}: {cart.product.name}</Typography>
+              <Typography>{t('order.quantity')}: {cart.Quantity}</Typography>
+              <Typography>{t('order.price')}: {cart.product.packageCost}</Typography>
+            </Card>
+          ))}
+        </Box>
+        <Typography variant="h5">{t('order.Total Amount')}: {amount}</Typography>
+
+        <form onSubmit={handleSubmit(saveDetails)}>
+          <FormControl>
+            <Typography>{t('order.How would you like to receive the shipment')}</Typography>
+            <RadioGroup
+              row
+              aria-labelledby="shipment-method-label"
+              name="shipment-method"
+              onChange={handleChange}
+              sx={{ justifyContent: 'center' }}
             >
-              <TextField
-                id='filled-basic'
-                fullWidth
-                label={t('order.City')}
-                variant='filled'
-                {...register('city')}
-              />
-              {errors.city && <p>{errors.city.message}</p>}
-              <TextField
-                id='filled-basic'
-                fullWidth
-                label={t('order.Street')}
-                variant='filled'
-                {...register('street')}
-              />
-              {errors.street && <p>{errors.street.message}</p>}
-              <TextField
-                id='filled-basic'
-                fullWidth
-                label={t('order.Building Number')}
-                variant='filled'
-                type='number'
-                {...register('buildingNumber')}
-              />
-              {errors.buildingNumber && <p>{errors.buildingNumber.message}</p>}
-              <TextField
-                id='filled-basic'
-                fullWidth
-                label={t('order.Floor')}
-                variant='filled'
-                type='number'
-                {...register('floor')}
-              />
-              {errors.floor && <p>{errors.floor.message}</p>}
-              <TextField
-                id='filled-basic'
-                fullWidth
-                label={t('order.Apartment Number')}
-                variant='filled'
-                type='number'
-                {...register('apartmentNumber')}
-              />
-              {errors.apartmentNumber && <p>{errors.apartmentNumber.message}</p>}
-              <TextField
-                id='filled-basic'
-                fullWidth
-                label={t('order.Last Name')}
-                variant='filled'
-                {...register('lastName')}
-              />
-              {errors.lastName && <p>{errors.lastName.message}</p>}
-              <Button type='submit' variant='contained' color='primary'>
-                {t('order.Save Details')}
-              </Button>
-            </Box>
-          )}
-        </FormControl>
-      </form>
-      
+              <FormControlLabel value="delivery" control={<Radio />} label={t('inorder.Shippg')} />
+              <FormControlLabel value="selfCollection" control={<Radio />} label={t('order.Self Collection')} />
+            </RadioGroup>
+
+            {selectedOption === 'delivery' && (
+              <Box sx={{ '& > :not(style)': { m: 1, width: '25ch', display: 'block' } }}>
+                <TextField id="city" fullWidth label="עיר" variant="filled" {...register('city')} />
+                {errors.city && <p>{errors.city.message}</p>}
+                <TextField id="street" fullWidth label="רחוב" variant="filled" {...register('street')} />
+                {errors.street && <p>{errors.street.message}</p>}
+                <TextField id="numBuild" fullWidth label="מספר בנין" variant="filled" type="number" {...register('numBuild')} />
+                {errors.numBuild && <p>{errors.numBuild.message}</p>}
+                <TextField id="floor" fullWidth label="קומה" variant="filled" type="number" {...register('floor')} />
+                {errors.floor && <p>{errors.floor.message}</p>}
+                <TextField id="apartmentNumber" fullWidth label="מספר דירה" variant="filled" type="number" {...register('apartmentNumber')} />
+                {errors.apartmentNumber && <p>{errors.apartmentNumber.message}</p>}
+                <TextField id="lastName" fullWidth label="שם משפחה" variant="filled" {...register('lastName')} />
+                {errors.lastName && <p>{errors.lastName.message}</p>}
+              </Box>
+            )}
+
+            <Button type="submit" variant="contained" color="primary">
+              {t('order.Save Details')}
+            </Button>
+          </FormControl>
+        </form>
+      </Grid>
+
       <Toast
         open={snackbarOpen}
         severity={snackbarSeverity}
