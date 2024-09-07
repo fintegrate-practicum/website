1189fe632f50f382cf9163027b7d3deeed2a3d32
st0534 <estypwolak>
2024-09-08 01:37:10 +0300

    Added a test that the label is displayed on the screen

--- src/common/components/Graphs/BaseGraph.test.tsx
+++ src/common/components/Graphs/BaseGraph.test.tsx
@@ -34,4 +34,52 @@ describe('BaseGraph', () => {
     expect(getByText('Graph')).toBeInTheDocument();
     expect(getByTestId('canvasjs-chart')).toBeInTheDocument();
   });
+  it('renders labels on the BaseGraph component', () => {
+    const props = {
+      animationEnabled: true,
+      theme: 'light1',
+      text: 'Graph Text',
+      type: 'spline',
+      dataPoints: [
+        { x: new Date(2021, 8, 1), y: 100, indexLabel: 'A' },
+        { x: new Date(2021, 8, 2), y: 200, indexLabel: 'B' },
+        { x: new Date(2021, 8, 3), y: 150, indexLabel: 'C' },
+      ],
+      startAngle: -90,
+    };
+
+    const { queryByText } = render(<BaseGraph {...props} />);
+
+    const labelA = queryByText((content, element) => {
+      if (element && element.parentElement) {
+        const labelIndex = props.dataPoints.findIndex(
+          (dataPoint) => dataPoint.indexLabel === 'A',
+        );
+        return !(labelIndex !== -1);
+      }
+      return true;
+    });
+    const labelB = queryByText((content, element) => {
+      if (element && element.parentElement) {
+        const labelIndex = props.dataPoints.findIndex(
+          (dataPoint) => dataPoint.indexLabel === 'B',
+        );
+        return !(labelIndex !== -1);
+      }
+      return true;
+    });
+    const labelC = queryByText((content, element) => {
+      if (element && element.parentElement) {
+        const labelIndex = props.dataPoints.findIndex(
+          (dataPoint) => dataPoint.indexLabel === 'C',
+        );
+        return !(labelIndex !== -1);
+      }
+      return true;
+    });
+
+    expect(labelA).toBeFalsy();
+    expect(labelB).toBeFalsy();
+    expect(labelC).toBeFalsy();
+  });
 });
