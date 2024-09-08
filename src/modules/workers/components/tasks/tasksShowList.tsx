87880a2f748a9b68e2dce9e8dc354045e3cf0e50
efchbd1013 <efchbd1013@gmail.com>
2024-09-08 17:35:58 +0300

    singleTask

--- src/modules/workers/components/tasks/tasksShowList.tsx
+++ src/modules/workers/components/tasks/tasksShowList.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import GenericList from '../../../../components/generic/genericList';
 import Task from '../../classes/task';
 import { useAppSelector } from '../../../../Redux/hooks';
+import SingleTask from './singleTask';
 
 interface ShowTaskListProps {
   filteredTasks: Task[];
@@ -41,6 +42,7 @@ const TasksShowList: React.FC<ShowTaskListProps> = ({
             { name: 'urgency', header: 'דחיפות', type: 'text' },
           ]}
           desing={null}
+          DialogComponent={SingleTask}
         />
       </div>
     </>
